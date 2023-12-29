f38f6a80d087d588ddea87c850ed831dac5ae980
moudlajs <pogoroll@gmail.com>
2023-12-29 02:14:09 +0100

    TF - fix indentation

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -1,8 +1,8 @@
 ﻿terraform {
-	backend "azurerm" {
-		resource_group_name		= "weatherforecast-rg"
-		storage_account_name	= "weatherforecastsa"
-		container_name			= "tfstate"
-		key						= "terraform.tfstate"
-	}
+  backend "azurerm" {
+	resource_group_name		= "weatherforecast-rg"
+	storage_account_name	= "weatherforecastsa"
+	container_name			= "tfstate"
+	key						= "terraform.tfstate"
+  }
 }
