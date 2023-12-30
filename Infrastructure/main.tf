bccdc46e4f431553f59ae8c36756223ba5b785ca
moudlajs <pogoroll@gmail.com>
2023-12-30 19:25:58 +0100

    Fix naming

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -4,8 +4,8 @@
 
 resource "azurerm_container_group" "container_group" {
   name					= "weatherforecast-cg"
-  location				= azurerm_resource_group.resource_group.location
-  resource_group_name	= azurerm_resource_group.resource_group.name
+  location				= "westeurope"
+  resource_group_name	= "tfstate"
 
   ip_address_type		= "Public"
   dns_name_label		= "weatherforecast"
